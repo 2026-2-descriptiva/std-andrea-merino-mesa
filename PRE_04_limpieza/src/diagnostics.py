f74c3580f3b326ea404b502f7cb4467453d3f284
Andrea Merino Mesa <amerino@unal.edu.co>
2026-09-18 18:48:16 -0500

    Taller 04 - Fechas
    Limpiar y mostrar fechas

--- PRE_04_limpieza/src/diagnostics.py
+++ PRE_04_limpieza/src/diagnostics.py
@@ -5,12 +5,17 @@ OUTPUT_FILE = "PRE_04_limpieza/submission/ventas.csv"
 
 def main():
     df = pd.read_csv(OUTPUT_FILE)
-    series = df["supplier"]
+    series = df["purchase_date"]
+
+    # series = series[series.str.contains(r"-\d{2}$", regex=True)]
+
     series = series.sort_values()
     series = series.drop_duplicates()
+
     print(series)
     print(len(series))
 
 
 if __name__ == "__main__":
     main()
+    
